e058fc5240095abaffbaaccaa82010d8061a3cf1
Nitish Goyal <nitish.goyal@phonepe.com>
2019-05-23 17:10:51 +0530

    Added logging

--- src/main/java/com/platform/callback/resources/CallbackResource.java
+++ src/main/java/com/platform/callback/resources/CallbackResource.java
@@ -81,6 +81,8 @@ public class CallbackResource {
                     .build();
             val mailboxTtl = HeaderUtil.getTTL(callbackRequest);
             val apiMap = RevolverBundle.matchPath(callbackRequest.getService(), callbackRequest.getApi());
+            log.info("Callbackservice : " + callbackRequest.getService() + ", api : " + callbackRequest.getApi() + ", api : " +
+                     apiMap.getApi());
             downstreamResponseHandler.saveResponse(requestId, response, callbackRequest.getMode(), mailboxTtl, apiMap.getApi());
             log.info("Callback added in the queue for processing");
             return Response.accepted()
